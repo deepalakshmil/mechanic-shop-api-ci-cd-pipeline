c9a79d151acfb46fe80e4d98f6b54592155a90e5
Deepalakshmi Lakshminarasimhan <writemail.deepa@gmail.com>
2025-12-10 16:48:11 -0500

    Fix: load_dotenv only in development

--- flask_app.py
+++ flask_app.py
@@ -3,10 +3,19 @@ from application import create_app
 from application.models import db
 import os
 
-load_dotenv()  # load .env file locally ##  # this loads .env automatically
-# Always call load_dotenv() locally before creating the app. 
+
+
+load_dotenv()   # Load .env only when running locally 
+                # This loads .env automatically
+                # Always call load_dotenv() locally before creating the app. 
+
+
+
+
 #app = create_app('ProductionConfig')   
 app = create_app("ProductionConfig" if os.environ.get("FLASK_ENV") == "production" else "DevelopmentConfig")
+    ## Render will added in environment variable use this (you should set FLASK_ENV=production in Render)
+
 
 
 
@@ -16,8 +25,8 @@ with app.app_context():
 
 # # Run locally
 #if __name__ == "__main__":
-   # app.run() => remove this line because run the app using gunicorn in production
-   # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run() => remove this line because run the app using gunicorn in production
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 ## gunicorn command to run the app in production environment and neccsccary to change app:app => flask_app:app
 
@@ -25,9 +34,7 @@ with app.app_context():
 Notes:
 
 load_dotenv() ensures local .env is loaded.
-
 FLASK_ENV controls whether to use Development or Production config.
-
 PORT environment variable is used for Render deployment.
 
 '''
